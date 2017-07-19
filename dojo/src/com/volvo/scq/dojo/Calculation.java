c95e903ee8b0f3bdbfe9bd12bead762864d149c7
Beata Kalis <beata.kalis@volvo.com>
2017-07-19 15:29:33 +0200

    Add books to basket

--- dojo/src/com/volvo/scq/dojo/Calculation.java
+++ dojo/src/com/volvo/scq/dojo/Calculation.java
@@ -7,12 +7,15 @@ public class Calculation {
     
     final static int PRICE = 8;
     
-    private String[] books = {"one","two","three","four","five"};
-    
     private List<Integer> basket = new ArrayList<Integer>();
 
     public int basketSize() {
         return basket.size();
     }
+    
+    void addBook(List<Integer> book) {
+        basket.addAll(book);
+    }
+    
     
 }
--- dojo/src/com/volvo/scq/dojo/CalculationTest.java
+++ dojo/src/com/volvo/scq/dojo/CalculationTest.java
@@ -2,16 +2,19 @@ package com.volvo.scq.dojo;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class CalculationTest {
-    
+
     @Test
     public void testBasics() {
-        
+
         Calculation calculation = new Calculation();
-        
+        assertEquals(0, calculation.basketSize());
+        calculation.addBook(Arrays.asList(1));
         assertEquals(1, calculation.basketSize());
-        
+
     }
 }
