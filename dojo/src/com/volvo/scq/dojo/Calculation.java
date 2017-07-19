16b0bbd0cd6633378107a5aa6f94be7d2a7a76d9
Pawel Fras <pawel.fras@volvo.com>
2017-07-19 15:55:50 +0200

    calculatePrice method has been added

--- dojo/src/com/volvo/scq/dojo/Calculation.java
+++ dojo/src/com/volvo/scq/dojo/Calculation.java
@@ -4,18 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculation {
-    
+
     final static int PRICE = 8;
-    
+
+    final static float discountValues[] = { 1, 0.95f, 0.90f, 0.80f, 0.75f };
+
     private List<Integer> basket = new ArrayList<Integer>();
 
     public int basketSize() {
         return basket.size();
     }
-    
-    void addBook(List<Integer> book) {
+
+    public void addBook(List<Integer> book) {
         basket.addAll(book);
     }
+
+    public float calculatePrice() {
+        switch (this.basketSize()) {
+        case 0:
+            return 0;
+        case 1:
+            return PRICE;
+        default:
+            return calculateDiscount();
+        }
+    }
     
-    
+    public float calculateDiscount() {
+        
+        return 0;
+    }
+
 }
--- dojo/src/com/volvo/scq/dojo/CalculationTest.java
+++ dojo/src/com/volvo/scq/dojo/CalculationTest.java
@@ -1,20 +1,34 @@
 package com.volvo.scq.dojo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculationTest {
 
+    private Calculation calculation;
+
+    @Before
+    public void setUp() {
+        calculation = new Calculation();
+    }
+
     @Test
     public void testBasics() {
 
-        Calculation calculation = new Calculation();
         assertEquals(0, calculation.basketSize());
         calculation.addBook(Arrays.asList(1));
         assertEquals(1, calculation.basketSize());
 
+    }
+
+    @Test
+    public void testCalculating() {
+       
+        
+        assertEquals(8, calculation.calculatePrice(), 0.0005);
     }
 }
